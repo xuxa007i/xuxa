7e5069d259fd7973c2b24e4f5e109ac5b59f2883
xuxa007i <144041880+xuxa007i@users.noreply.github.com>
2023-11-01 12:15:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 function tocaSom(idElementoAudio){
- document.querySelector(idElementoAudio).play();
-}
-const listaDeTeclas=document.querySelectorAll('.tecla');
 
-let contador = 0;
+const elemento = document.querySelector(idElementoAudio);
+
+if(elemento === null) {
+alert('Elemento não encontrado')
+ }
+}
+const listaDeTeclas = document.querySelectorAll('.tecla')
 //Estrutura de repetição - Enquanto
 for(let contador = 0; contador < listaDeTeclas.leigth;contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_ ${instrumento}`;
+
     console.log(idAudio);
 
     tecla.onclick = function () {
        tocaSom(idAudio);
     };
-    tecla.onkeydown = function(){
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Enter' || evento.code === 'Space'){
        tecla.classList.add('ativa');
     }
+};
     tecla.onkeyup = function(){
         tecla.classList.remove('ativa');
-    }
+    };
+       console.log(instrumento);
     console.log(contador);
-}
+};
 
     
 
